c771f2a1bdfc6e7a4b7a72a0ddc830e67fe67b2c
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-10-10 20:43:07 -0400

    Create full deck, shuffle, dealing, player decks, game logic

---
+++ master.js
@@ -0,0 +1,203 @@
+/*jshint esversion: 6 */
+/*
+
+PSEUDOCODE
+
+Goal:
+To be the first player to win all 52 cards. Therefore When one player has 52 cards the game ends.
+Put another way: when one of the player arrays has an 'array.length' of 52 the game ends.
+-
+-
+Dealing:
+1. deck with 52 cards
+2. shuffle that deck
+3. split that deck into two equal parts
+
+
+-
+-
+Playing:
+Each player turns up a card at the same time.
+The player with the higher card takes both cards and puts them, face down, on the bottom of their stack.
+If the cards are the same rank, it is war
+Each player flips another card --> the higher card wins
+If these cards are the same rank it is war
+Each player flips another card --> the higher card wins
+And so on until one player wins and then they take all the cards.
+-
+-
+Scoring:
+Keep tracking of the number of cards each player has through out the game.
+-
+-
+What to Code:
+Create 52 cards;
+Split the 52 cards into two arrays randomly, so that each array has 26 cards
+deckArray = 52 cards
+randomly distribute cards
+player1 array = 26 cards
+player2 array = 26 cards
+
+*/
+
+//Shuffling the deck
+
+
+var deck = [
+  {suit: "Hearts", card: "Ace", rank: 1},
+  {suit: "Hearts", card: "2", rank: 2},
+  {suit: "Hearts", card: "3", rank: 3},
+  {suit: "Hearts", card: "4", rank: 4},
+  {suit: "Hearts", card: "5", rank: 5},
+  {suit: "Hearts", card: "6", rank: 6},
+  {suit: "Hearts", card: "7", rank: 7},
+  {suit: "Hearts", card: "8", rank: 8},
+  {suit: "Hearts", card: "9", rank: 9},
+  {suit: "Hearts", card: "10", rank: 10},
+  {suit: "Hearts", card: "Jack", rank: 11},
+  {suit: "Hearts", card: "Queen", rank: 12},
+  {suit: "Hearts", card: "King", rank: 13},
+
+  {suit: "Spades", card: "Ace", rank: 1},
+  {suit: "Spades", card: "2", rank: 2},
+  {suit: "Spades", card: "3", rank: 3},
+  {suit: "Spades", card: "4", rank: 4},
+  {suit: "Spades", card: "5", rank: 5},
+  {suit: "Spades", card: "6", rank: 6},
+  {suit: "Spades", card: "7", rank: 7},
+  {suit: "Spades", card: "8", rank: 8},
+  {suit: "Spades", card: "9", rank: 9},
+  {suit: "Spades", card: "10", rank: 10},
+  {suit: "Spades", card: "Jack", rank: 11},
+  {suit: "Spades", card: "Queen", rank: 12},
+  {suit: "Spades", card: "King", rank: 13},
+
+  {suit: "Diamonds", card: "Ace", rank: 1},
+  {suit: "Diamonds", card: "2", rank: 2},
+  {suit: "Diamonds", card: "3", rank: 3},
+  {suit: "Diamonds", card: "4", rank: 4},
+  {suit: "Diamonds", card: "5", rank: 5},
+  {suit: "Diamonds", card: "6", rank: 6},
+  {suit: "Diamonds", card: "7", rank: 7},
+  {suit: "Diamonds", card: "8", rank: 8},
+  {suit: "Diamonds", card: "9", rank: 9},
+  {suit: "Diamonds", card: "10", rank: 10},
+  {suit: "Diamonds", card: "Jack", rank: 11},
+  {suit: "Diamonds", card: "Queen", rank: 12},
+  {suit: "Diamonds", card: "King", rank: 13},
+
+  {suit: "Clubs", card: "Ace", rank: 1},
+  {suit: "Clubs", card: "2", rank: 2},
+  {suit: "Clubs", card: "3", rank: 3},
+  {suit: "Clubs", card: "4", rank: 4},
+  {suit: "Clubs", card: "5", rank: 5},
+  {suit: "Clubs", card: "6", rank: 6},
+  {suit: "Clubs", card: "7", rank: 7},
+  {suit: "Clubs", card: "8", rank: 8},
+  {suit: "Clubs", card: "9", rank: 9},
+  {suit: "Clubs", card: "10", rank: 10},
+  {suit: "Clubs", card: "Jack", rank: 11},
+  {suit: "Clubs", card: "Queen", rank: 12},
+  {suit: "Clubs", card: "King", rank: 13},
+];
+
+function shuffle(array){
+  var m = array.length, t, i;
+  while(m){
+    i = Math.floor(Math.random()* m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+}
+  console.log(shuffle(deck));
+
+//Distributing the deck to the two players - each player should have 26 cards
+
+/* player 1's hand */ var p1 = [];
+/* player 2's hand */ var p2 = [];
+
+p1 = deck.slice(0,26);
+p2 = deck.slice(26);
+console.log(p1);
+console.log(p2);
+
+// Playing the game
+
+// /*if both players have cards, the game should be in play */
+
+//play game
+
+// while(p1.length > 0 && p2.length > 0){ }
+
+/*Player one pulls the card from the top of the deck (i.e. the last object in the player 1 array).
+  Player two pulls the card from the top of the deck (i.e the last object in the player 2 array ).
+*/
+var warPile = [];
+var p1Card = p1.pop();
+var p2Card = p2.pop();
+
+/*Tell each player what card they drew*/
+
+console.log("Player 1: ");
+console.log(p1Card);
+console.log("Player 2: ");
+console.log(p2Card);
+
+/*Compare the ranks of the cards.
+If player 1's card is greater than player 2, player 1 wins - player 1 gets both played cards; reset the played cards variable to empty
+If player 2's card is greater than player 1, player 2 wins - player 2 gets both played cards; reset the played cards variable to empty
+Or else, the game goes into WAR. */
+
+if( p1Card.rank > p2Card.rank){
+  console.log("Player 1 wins round");
+  p1.unshift(p1Card, p2Card);
+
+  console.log(p1);
+  console.log(p2);
+  p1Card = 0;
+  p2Card = 0;
+}
+else if(p2Card.rank > p1Card.rank){
+  console.log("Player 2 wins round");
+  p2.unshift(p1Card, p2Card);
+
+  console.log(p2);
+  console.log(p1);
+  p1Card = 0;
+  p2Card = 0;
+
+}
+else{
+  console.log("War!");
+  if(p1.length > 3 && p2.length > 3){
+  //pull 3 cards from the end of player 1 deck
+  //pull 3 cards from the end of player 2 deck
+    p1Card = p1.pop();
+    warPile.push(p1Card);
+    p1Card = p1.pop();
+    warPile.push(p1Card);
+    p1Card = p1.pop();
+    warPile.push(p1Card);
+
+    p2Card = p2.pop();
+    warPile.push(p2Card);
+    p2Card = p2.pop();
+    warPile.push(p1Card);
+    p2Card = p2.pop();
+    warPile.push(p2Card);
+    console.log(warPile);
+
+
+
+  //play the game again, who ever wins the next round gets the 2 cards in play,
+  //plus all 6 cards being held in pile.
+  }
+}
+
+
+
+// if player 1's card (with index 0) is greater than player 2's card (with index 1), than player 1 wins.
+// if player 2's card (with index 1) is greater than player 1's card (with index 0), than player 2 wins.
+// or else, the game goes into war
